dd8bbf94367e021609adf315ccd7a0c376de99bb
Johannes Koester <johannes.koester@uni-due.de>
2026-02-11 12:24:27 +0100

    initial code

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Varlociraptor Inspect")
+st.text("Visual inspection of Varlociraptor VCF records.")
+
+# load record, either from text_input, or from URL params
+record = st.text_input('Paste your Varlociraptor VCF record here')
+# parse (you will have to add a header) and plot
